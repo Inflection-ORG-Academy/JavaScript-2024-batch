8eddbb3be3032de0d623bf2122fdbbe723c5b02c
Deepak Kumar Maurya <deepak04122000maurya@gmail.com>
2024-09-06 16:29:12 +0530

    created a table program using for loop

---
+++ Programs/2.Table.js
@@ -0,0 +1,9 @@
+const tab = 28;
+
+for (let i = 1; i <= 10; i = i + 1) {
+  // console.log(tab * i);
+
+  // String Concatination
+  const finalOutput = tab + 'x' + i + '=' + tab * i;
+  console.log(finalOutput);
+}
