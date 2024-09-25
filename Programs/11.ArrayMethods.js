db2158be0fa3ef50688f69fb45e92d8aa0890b2e
Deepak Kumar Maurya <deepak04122000maurya@gmail.com>
2024-09-25 09:05:09 +0530

    Array forEach Method

---
+++ Programs/11.ArrayMethods.js
@@ -0,0 +1,8 @@
+// All Array Methods
+
+const namesArr = ['Rohan', 'Sohan', 'Shiv', 'Prakash', 'Monika'];
+
+// Iterate Array using Array "forEach" Method
+namesArr.forEach((el) => {
+  console.log(el);
+});
