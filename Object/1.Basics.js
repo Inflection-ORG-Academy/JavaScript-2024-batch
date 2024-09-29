2cba4029ced1f8f1b475a6241b873b5b6cfdacec
Deepak Kumar Maurya <deepak04122000maurya@gmail.com>
2024-09-29 09:02:54 +0530

    Basic Object

--- Object/1.Basics.js
+++ Object/1.Basics.js
@@ -23,3 +23,25 @@ const person = student;
 person.sirName = 'Maurya';
 console.log(person);
 console.log(student);
+
+// Checking Day By Number
+function checkDay(num) {
+  if (num < 1 || num > 7) {
+    console.log('Provide Valid Number');
+    return;
+  }
+
+  const days = {
+    1: 'Sunday',
+    2: 'Monday',
+    3: 'Tuesday',
+    4: 'Wednesday',
+    5: 'Thursday',
+    6: 'Friday',
+    7: 'Saturday',
+  };
+
+  console.log(days[num]);
+}
+
+checkDay(7);
